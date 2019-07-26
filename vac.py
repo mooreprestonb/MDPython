9542bf4edbec60ef06231d6f4a2c436a1663781d
mooreprestonb <mooreprestonb@gmail.com>
2019-07-25 20:00:07 -0400

    Another speadup...

--- vac.py
+++ vac.py
@@ -13,7 +13,7 @@ lines = file.readlines()
 nwindow = 2**10
 
 print("Called with:",sys.argv)
-print("First line of",sys.argv[0],lines[0])
+print("First line of",sys.argv[0],lines[0].rstrip())
 print("Window length:",nwindow)
 
 natoms = lines[0].split()[1]
@@ -21,36 +21,43 @@ natoms = int(natoms)
 dt = float(lines[natoms+1].split()[3]) - float(lines[0].split()[3])
 print(natoms,nwindow,dt)
 
-nconf = len(lines)//(natoms+1)
+nconf = int(len(lines)/(natoms+1))
 print("Number of configurations: ",nconf)
 
-data = numpy.zeros((nwindow,natoms,3))
+data = numpy.zeros((nconf,natoms,3))
 vac = numpy.zeros(nwindow)
 
 # print(lines[0:10])
-for i in range(nwindow): # get initial data
+#for i in range(nwindow): # get initial data
+#    for j in range(natoms):
+#        line  = lines[i*(natoms+1)+j+1].split()
+#        #print(i,j,line)
+#        data[i][j] = line
+# get data
+for i in range(nconf):
     for j in range(natoms):
-        line  = lines[i*(natoms+1)+j+1].split()
-        #print(i,j,line)
-        data[i][j] = line
+        data[i][j] = lines[i*(natoms+1)+j+1].split()
 
-itime = 10
+itime = 5
 tnow = time.time()
 ttime = tnow
 
 print("Correlating")
 for i in range(nconf-nwindow):
     # Correlate
-    vac += numpy.einsum('ijk,jk',data,data[0])
+    vac += numpy.einsum('ijk,jk',data[i:i+nwindow],data[i])
     #for k in range(nwindow):
     #    for j in range(natoms):
     #        vac[k] += numpy.dot(data[k][j],data[0][j])
-            
+    ##       sum = 0
+    ##       for l in range(3):
+    ##            sum += data[k][j][l]*data[0][j][l]
+    ##       vac[k] += sum
     # get next data window
-    for j in range(nwindow-1): # move array down
-        data[j] = data[j+1]
-    for j in range(natoms): # read data into end of array
-        data[-1][j] = lines[i*(natoms+1)+j+1].split()
+    #for j in range(nwindow-1): # move array down
+    #    data[j] = data[j+1]
+    #for j in range(natoms): # read data into end of array
+    #    data[-1][j] = lines[i*(natoms+1)+j+1].split()
 
     if(itime < time.time()-tnow): # report where we are
         print('conf = {}/{} = {:.4f}%, time = {:g}'.format(i,nconf,i/nconf*100,time.time()-ttime))
